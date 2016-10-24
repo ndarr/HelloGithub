fa4396da2114bc97d53e7442c50e044d1be71fd9
ndarr <ndarr@stud.fra-uas.de>
2016-10-24 14:05:24 +0200

    First Changes made: sum of a +b Some bugfixes lulz

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Github");
+        int a, b ;
+        a = 1;
+        b = 2;
+        int c = a + b;
     }
 }
